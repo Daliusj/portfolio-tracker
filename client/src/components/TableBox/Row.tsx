499fe4fd2d42030fc3ec1523af964be5afae06c6
Dalius Juocas <dalius.juocas@gmail.com>
2024-08-15 12:00:21 +0300

    feat(client): add currency symbols to stats table

--- client/src/components/TableBox/Row.tsx
+++ client/src/components/TableBox/Row.tsx
@@ -1,31 +1,48 @@
 import { Table } from 'flowbite-react'
 import React, { useId } from 'react'
-import { FullPortfolioGroupedPublic } from '@server/shared/types'
+import { PortfolioStatsPublic } from '@server/shared/types'
+import { HiOutlineArrowSmUp, HiOutlineArrowSmDown } from 'react-icons/hi'
+import getSymbolFromCurrency from 'currency-symbol-map'
+import { usePortfolio } from '@/context/PortfolioContext'
 
 type TableRowProps = {
-  asset: FullPortfolioGroupedPublic
+  asset: PortfolioStatsPublic
 }
 export default function ({ asset }: TableRowProps) {
   const cellStyles = 'font-small whitespace-no-wrap items-center text-gray-900 dark:text-white'
-
-  const totalQuantity = asset.purchases.reduce(
-    (acc, purchase) => acc + Number(purchase.quantity),
-    0
-  )
-  const avgBuyPrice =
-    asset.purchases.reduce((acc, purchase) => acc + Number(purchase.purchasePrice), 0) /
-    totalQuantity
-
-  const totalValue = Number(asset.assetPrice) * totalQuantity
+  const { activePortfolio } = usePortfolio()
 
   return (
     <Table.Row className={` bg-white dark:border-gray-700 dark:bg-gray-800 `} key={useId()}>
-      <Table.Cell className={cellStyles}>{asset.assetName}</Table.Cell>
-      <Table.Cell className={cellStyles}>{totalQuantity}</Table.Cell>
-      <Table.Cell className={cellStyles}>{avgBuyPrice}</Table.Cell>
-      <Table.Cell className={cellStyles}>Allocation</Table.Cell>
-      <Table.Cell className={cellStyles}>{asset.assetPrice}</Table.Cell>
-      <Table.Cell className={cellStyles}>{totalValue}</Table.Cell>
+      <Table.Cell className={cellStyles}>{asset.name}</Table.Cell>
+      <Table.Cell className={cellStyles}>{asset.totalQuantity}</Table.Cell>
+      <Table.Cell
+        className={cellStyles}
+      >{`${getSymbolFromCurrency(asset.assetCurrencyCode)}${asset.avgBuyPrice}`}</Table.Cell>
+      <Table.Cell className={cellStyles}>{asset.allocation}</Table.Cell>
+      <Table.Cell className={cellStyles}>{asset.price}</Table.Cell>
+      <Table.Cell className={cellStyles}>
+        <div>{`${getSymbolFromCurrency(asset.assetCurrencyCode)}${asset.value}`}</div>
+        {activePortfolio && activePortfolio?.currencySymbol !== asset.assetCurrencyCode && (
+          <div>
+            {`(${getSymbolFromCurrency(activePortfolio?.currencySymbol)}${asset.valueInBaseCurrencie})`}
+          </div>
+        )}
+      </Table.Cell>
+      <Table.Cell className={cellStyles}>
+        <div
+          className={`flex items-center gap-1 ${Number(asset.valueChange) > 0 ? 'text-emerald-500' : 'text-orange-600'} `}
+        >
+          <div>
+            {Number(asset.valueChange) > 0 ? (
+              <HiOutlineArrowSmUp className="h-6 w-6 " />
+            ) : (
+              <HiOutlineArrowSmDown className="h-6 w-6 " />
+            )}
+          </div>
+          {`${asset.valueChange.replace('-', '')}\n${asset.percentageChange.replace('-', '')}%`}
+        </div>
+      </Table.Cell>
     </Table.Row>
   )
 }
--- client/src/components/TableBox/TableBox.tsx
+++ client/src/components/TableBox/TableBox.tsx
@@ -1,9 +1,26 @@
+import { Table } from 'flowbite-react'
 import React from 'react'
-import Table from './Table'
-import { usePortfolioAssets } from '@/context/portfolioAssets'
-import { FullPortfolioGroupedPublic } from '@server/entities/portfolio'
+import TableRow from './Row'
+import TableHead from './Head'
+import { usePortfolio } from '@/context/PortfolioContext'
+import { trpc } from '@/trpc'
 
-export default function TableBox() {
-  const assets = usePortfolioAssets()
-  return <Table assets={assets.data as FullPortfolioGroupedPublic[]} />
+export default function () {
+  const { activePortfolio } = usePortfolio()
+  const assetStats =
+    activePortfolio && trpc.portfolioStats.get.useQuery({ id: activePortfolio?.id })
+
+  return (
+    <div className="overflow-x-auto">
+      <Table hoverable>
+        <TableHead />
+        <Table.Body className="divide-y overflow-y-auto">
+          {assetStats?.data &&
+            assetStats.data.map((asset) => (
+              <TableRow key={`${asset.assetId}-table-box`} asset={asset} />
+            ))}
+        </Table.Body>
+      </Table>
+    </div>
+  )
 }
