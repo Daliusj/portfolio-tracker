15fef5a01081d4a752cc34dee3fd40fbaf1a273c
Dalius Juocas <dalius.juocas@gmail.com>
2024-08-19 18:39:08 +0300

    ci: fix workflow

--- server/tests/utils/database.ts
+++ server/tests/utils/database.ts
@@ -1,4 +1,5 @@
 import config from '@server/config'
 import { createDatabase } from '@server/database'
 
-export const createTestDatabase = () => createDatabase(config.testDatabase)
+export const createTestDatabase = () =>
+  createDatabase(process.env.CI ? config.database : config.testDatabase)
