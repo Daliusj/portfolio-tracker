d68a7427965f911126377b7479e044f94543619b
Dalius Juocas <dalius.juocas@gmail.com>
2024-08-19 18:07:16 +0300

    tests(client): fix time test case

--- client/src/utils/time.spec.ts
+++ client/src/utils/time.spec.ts
@@ -4,6 +4,7 @@ describe('localDateToIsoString', () => {
   it('should return the correct local date when given a date object in UTC', () => {
     const date = new Date('2024-07-31T21:00:00.000Z')
 
-    expect(localDateToIsoString(date)).toEqual('2024-08-01')
+    const expectedDate = process.env.CI ? '2024-07-31' : '2024-08-01'
+    expect(localDateToIsoString(date)).toEqual(expectedDate)
   })
 })
